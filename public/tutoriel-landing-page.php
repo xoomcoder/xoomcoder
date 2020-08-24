dec5755a05ee046805edf9602c5b4310f1bd7ad3
LH XoomCoder <github@xoomcoder.com>
2020-08-24 14:30:30 +0200

    ajout meta description

--- public/tutoriel-landing-page.php
+++ public/tutoriel-landing-page.php
@@ -3,7 +3,13 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="tutoriel pour coder une landing page">
     <title>Document</title>
+    <style>
+h1 {
+    color:#ba000d;
+}        
+    </style>
 </head>
 <body>
     <header>
