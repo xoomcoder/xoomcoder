d9288e5f0bfc342acdb7379417068c118a5829ac
LH XoomCoder <github@xoomcoder.com>
2020-08-25 10:33:17 +0200

    mise en place menu et navigation

---
+++ xoom-templates/contenu-tutoriels.php
@@ -0,0 +1,12 @@
+
+        <section>
+            <h2>Liste des tutoriels</h2>
+            <p>Maintenant on a découpé la page dans différents fichiers 😇</p>
+            <pre>
+Et avec une balise pre on peut garder les retours à la ligne sans code HTML.
+            </pre>
+            <article>
+                <a href="tutoriel-landing-page.php"><h3>Coder une Landing Page</h3></a>
+                <p>tutoriel pour coder une landing page</p>
+            </article>
+        </section>
