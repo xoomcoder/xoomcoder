a4c7afad12028571f42802fbdd58a3c996c9d9c5
LH XoomCoder <github@xoomcoder.com>
2020-08-25 11:19:47 +0200

    maéliorations css et html pour web.dev

--- xoom-templates/contenu-tutoriels.php
+++ xoom-templates/contenu-tutoriels.php
@@ -3,11 +3,15 @@
             <h1>Liste des tutoriels</h1>
             <p>Vous pouvez retrouver ici la liste des tutoriels pour les différents types de projets web.</p>
             <article>
-                <a href="tutoriel-landing-page.php"><h3>Coder une Landing Page</h3></a>
+                <a href="tutoriel-landing-page.php">
+                    <h2>Coder une Landing Page</h2>
+                </a>
                 <p>tutoriel pour coder une landing page</p>
             </article>
             <article>
-                <a href="tutoriel-landing-page.php"><h3>Coder un Site Vitrine</h3></a>
+                <a href="tutoriel-landing-page.php">
+                    <h2>Coder un Site Vitrine</h2>
+                </a>
                 <p>tutoriel pour coder un site vitrine</p>
             </article>
         </section>
