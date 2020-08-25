3020300e1fd4f2b3cd5bbbe50917ab54bb569795
LH XoomCoder <github@xoomcoder.com>
2020-08-25 10:45:59 +0200

    code js dans un fichier séparé site.js

--- xoom-templates/footer.php
+++ xoom-templates/footer.php
@@ -8,31 +8,9 @@
         <p>Des informations statistiques sur les visites sont stockées par l'hébergement.</p>
     </footer>
 
+    <script src="assets/js/site.js"></script>
     <script>
-// https://plainjs.com/javascript/events/running-code-when-the-document-is-ready-15/
-function run() {
-    // do something... 2000ms later
-
-    setTimeout(() => {
-        scripts = document.querySelectorAll('script[type="text/xoomcoder"]');
-        for(var s=0; s<scripts.length; s++)
-        {
-            var current = scripts[s];
-            // https://developer.mozilla.org/fr/docs/Web/API/Element/insertAdjacentHTML
-            current.insertAdjacentHTML('afterend', current.innerHTML);
-        }        
-    }, 2000);
-
-}
-
-// in case the document is already rendered
-if (document.readyState!='loading') run();
-// modern browsers
-else if (document.addEventListener) document.addEventListener('DOMContentLoaded', run);
-// IE <= 8
-else document.attachEvent('onreadystatechange', function(){
-    if (document.readyState=='complete') run();
-});        
+    
     </script>
 </body>
 </html>
