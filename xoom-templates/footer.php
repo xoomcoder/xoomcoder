9ff3fec4f3065b58c65aabae73ff0af2848dfd8e
LH XoomCoder <github@xoomcoder.com>
2020-08-26 17:57:31 +0200

    html footer

--- xoom-templates/footer.php
+++ xoom-templates/footer.php
@@ -1,8 +1,8 @@
 </main>
     <footer>
         <a href="?"><img loading="lazy" class="logo" src="assets/img/xoomcoder.svg" alt="xoomcoder.com"></a>
-        <h3><a href="?">XoomCoder</a></h3>
-        <p>tous droits réservés</p>
+        <p><a href="./">Revenir à l'accueil</a></p>
+        <p>XoomCoder * tous droits réservés</p>
         <p>&copy;2020</p>
         <p>Ce site est géré et publié par <a href="https://www.linkedin.com/in/applh">Long Hai LH</a> et le code est hébergé sur ionos.fr</p>
         <p>Des informations statistiques sur les visites sont stockées par l'hébergement.</p>
