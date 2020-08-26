a654adac7643ef6b0b9c5bf9cc1b757a86920768
LH XoomCoder <github@xoomcoder.com>
2020-08-26 23:51:09 +0200

    ajout headers pour email

--- xoom/class/Email.php
+++ xoom/class/Email.php
@@ -4,7 +4,16 @@ class Email
 {
     static function send ($to, $subject, $message)
     {
+        // ionos hosting put bad labels... 
+        $headers = [];
+        if (class_exists("Config") && isset(Config::$adminEmail)) {
+            $headers = [
+                'From'      => Config::$adminEmail,
+                'Reply-To'  => Config::$adminEmail,
+            ];
+        }
+
         // https://www.php.net/manual/fr/function.mail.php
-        @mail($to, $subject, $message);
+        @mail($to, $subject, $message, $headers);
     }
 }
