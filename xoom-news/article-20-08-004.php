d1637e075de3270831fc2bcd9df9923a2c7fc594
LH XoomCoder <github@xoomcoder.com>
2020-08-26 17:35:42 +0200

    ajout css et contenu sur 3 colonnes

--- xoom-news/article-20-08-004.php
+++ xoom-news/article-20-08-004.php
@@ -2,4 +2,7 @@
 <article>
     <h2>Level 3: Blog en POO sans BDD</h2>
     <p>Le site XoomCoder arrive maintenant au niveau 3. On ajoute en premier une page news pour les actualités.</p>
+    <p>Pour le SEO, il n'y a pas encore suffisamment de contenu dans chaque article pour les séparer dans une page à part. Pour le moment, tous les articles sont dans la page news.</p>
+    <p>Sur la page contact, il y a aussi un formulaire pour laisser un message. L'envoi du formulaire est réalisé en Ajax, sans chargement de la page.</p>
+    <p>Comme souvent en développement local, il n'y a pas de serveur d'email. Il y a maintenant la possibilité d'ajouter un code local pour personnaliser ces fonctionnalités. La Programmation Par Classes (PPC) permet de ranger facilement son code.</p>
 </article>
--- xoom-templates/contenu-news.php
+++ xoom-templates/contenu-news.php
@@ -1,5 +1,5 @@
 
-<section>
+<section class="x3col">
     <h1>News</h1>
     <?php News::show() ?>
 </section>
