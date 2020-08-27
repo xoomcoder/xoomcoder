8d05f8da6203cf49b1db10b97178b5f8dcf2e327
LH XoomCoder <github@xoomcoder.com>
2020-08-27 15:15:23 +0200

    ajout contenu

--- xoom-templates/contenu-formation.php
+++ xoom-templates/contenu-formation.php
@@ -1,4 +1,75 @@
 
 <section>
     <h1>Formation Fullstack / Développeur Web et Applications Mobiles</h1>
+    <article>
+        <h2>Site Landing Page</h2>
+        <pre>
+🔥 HTML 
+🔥 CSS 
+🔥 JS 
+        </pre>
+    </article>
+    <article>
+        <h2>Site Vitrine</h2>
+        <pre>
+🔥 HTML 
+🔥 CSS 
+🔥 JS 
+🔥 PHP 
+        </pre>
+    </article>
+    <article>
+        <h2>Site Blog</h2>
+        <pre>
+🔥 HTML 
+🔥 CSS 
+🔥 JS 
+🔥 PHP 
+🔥 SQL 
+        </pre>
+    </article>
+    <article>
+        <h2>Site CMS</h2>
+        <pre>
+🔥 HTML 
+🔥 CSS 
+🔥 JS 
+🔥 PHP 
+🔥 SQL 
+        </pre>
+    </article>
+    <article>
+        <h2>Site MarketPlace</h2>
+        <pre>
+🔥 HTML 
+🔥 CSS 
+🔥 JS 
+🔥 PHP 
+🔥 SQL 
+        </pre>
+    </article>
+    <article>
+        <h2>Site WebApp</h2>
+        <pre>
+🔥 HTML 
+🔥 CSS 
+🔥 JS 
+🔥 PHP 
+🔥 SQL 
+        </pre>
+    </article>
+    <article>
+        <h2>Application Mobile Android</h2>
+        <pre>
+🔥 Kotlin 
+🔥 Java 
+        </pre>
+    </article>
+    <article>
+        <h2>Application Mobile iOS</h2>
+        <pre>
+🔥 Swift 
+🔥 Objective-C 
+        </pre>
+    </article>
 </section>
