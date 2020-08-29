9204fe41a9251a9fbcd3a5f6710f4a5836ad82a1
LH XoomCoder <github@xoomcoder.com>
2020-08-29 21:18:34 +0200

    ajout page

--- xoom-templates/contenu-tutoriels.php
+++ xoom-templates/contenu-tutoriels.php
@@ -1,7 +1,7 @@
 
-<section>
+<section class="x3col">
     <h1>Liste des tutoriels</h1>
-    <p>Vous pouvez retrouver ici la liste des tutoriels pour les différents types de projets web.</p>
+    <p class="w100">Vous pouvez retrouver ici la liste des tutoriels pour les différents types de projets web.</p>
     <article>
         <a href="tutoriel-landing-page">
             <h2>Coder une Landing Page</h2>
@@ -25,6 +25,12 @@
             <h2>Coder un Site CMS</h2>
         </a>
         <p>tutoriel pour coder un site CMS</p>
+    </article>
+    <article>
+        <a href="tutoriel-site-marketplace">
+            <h2>Coder un Site Marketplace</h2>
+        </a>
+        <p>tutoriel pour coder un site Marketplace</p>
     </article>
 </section>
 
