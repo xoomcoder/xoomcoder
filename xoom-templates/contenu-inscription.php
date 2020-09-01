2d3cea14de7572ce4e352318ecd64a79af8e1f29
LH XoomCoder <github@xoomcoder.com>
2020-09-01 14:43:28 +0200

    ajout html pattern

--- xoom-templates/contenu-inscription.php
+++ xoom-templates/contenu-inscription.php
@@ -4,16 +4,16 @@
     <form action="api" method="POST">
         <!-- partie publique -->
         <label>
-            <div>votre login</div>
-            <input type="text" name="login" required placeholder="votre login">
+            <div>votre identifiant</div>
+            <input type="text" name="login" required placeholder="votre identifiant" pattern="^[a-z0-9][a-z0-9-]{1,20}[a-z0-9]$" maxlength="100">
         </label>
         <label>
             <div>votre email</div>
-            <input type="email" name="email" required placeholder="votre email">
+            <input type="email" name="email" required placeholder="votre email" maxlength="160">
         </label>
         <label>
             <div>votre mot de passe</div>
-            <input type="password" name="password" required placeholder="votre mot de passe">
+            <input type="password" name="password" required placeholder="votre mot de passe" pattern="^.{4,100}$" maxlength="100">
         </label>
         <button type="submit">Créer votre compte</button>
         <div class="feedback"></div>
