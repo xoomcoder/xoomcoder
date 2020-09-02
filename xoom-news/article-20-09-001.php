f6292215c5e71f38a79b57f43301e7067c8e4301
LH XoomCoder <github@xoomcoder.com>
2020-09-02 15:52:50 +0200

    ajout article

--- xoom-news/article-20-09-001.php
+++ xoom-news/article-20-09-001.php
@@ -1,4 +1,26 @@
 <article class="">
+    <h2>Level 5: PHPMailer pour envoyer des mails simplement</h2>
+    <p>
+    Les emails restent un moyen de communication essentiel pour un projet internet.
+    Création de compte, mot de passe oublié, newsletter, notifications, etc...
+    La plupart des hébergements mutualisés donnent un quota limite à surveiller si vous avez besoin d'envoyer plusieurs milliers de mails par jour. Au delà, il faut passer sur un serveur dédié ou un service spécialisé.    
+    </p>
+    <p>
+    PHPMailer est une classe PHP pour envoyer des mails plus complexes (en HTML, avec des pièces jointes, etc...), aussi facilement que la fonction mail disponible dans PHP. Le code est organisé en une classe PHPMailer et quelques autres classes optionnelles. Il est ainsi possible de ne prendre qu'un seul fichier qui comprend autour de 5000 lignes de code (avec plein de commentaires 😇).
+    Cette simplicité fait que depuis 20 ans, cette classe est aussi intégrée dans de nombreux frameworks ou CMS du monde PHP.
+    </p>
+    <p>
+    https://github.com/PHPMailer/PHPMailer
+    </p>
+    <p>
+    Il est aussi possible d'utiliser Composer pour ajouter ce module avec d'autres modules dans votre projet. C'est un bon module simple pour commencer à utiliser Composer.
+    Composer ajoute un fichier vendor/autoload.php à insérer dans votre projet, au début de votre code. Les différents modules sont dans le dossier vendor/. 
+    Composer est assez lourd en ressources consommées et peut installer beaucoup de fichiers sans que vous vous en rendiez compte... A surveiller.    
+    </p>
+</article>
+
+
+<article class="">
     <h2>Blood Machines: de la french SF fraîche.</h2>
     <p>
     Dans les sorties ciné de la semaine, il y a Blood Machines, épisode de 1H de SF réalisé par des français.
