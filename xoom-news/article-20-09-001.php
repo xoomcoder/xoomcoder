8108416b4901f079ec6c10ab1b5f7c8d891841be
LH XoomCoder <github@xoomcoder.com>
2020-09-02 15:01:09 +0200

    ajout article

---
+++ xoom-news/article-20-09-001.php
@@ -0,0 +1,25 @@
+<article class="">
+    <h2>Blood Machines: de la french SF fraîche.</h2>
+    <p>
+    Dans les sorties ciné de la semaine, il y a Blood Machines, épisode de 1H de SF réalisé par des français.
+    Visuellement très impressionnant et plein de références à d'autres univers connus.
+    Le making-of est toujours un plaisir pour découvrir les techniques qui se composent pour créer une oeuvre multimedia.
+    Le cinema mélange le réel avec des bricolages et des maquettes, des acteurs devant écrans verts jusqu'aux créations virtuelles en 3D gigantesques.
+    Et la bande son envoûte les spectateurs et participe à l'expérience presque hypnotique, entre la danse et la transe.
+    L'univers de Blood Machines reprend l'ambiance d'un autre clip, intitulé Turbo Killer.
+    </p>
+</article>
+<script type="text/xoomcoder">
+<iframe width="560" height="315" src="https://www.youtube.com/embed/jLHhr8Xc4AM" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+</script>
+<script type="text/xoomcoder">
+<iframe width="560" height="315" src="https://www.youtube.com/embed/WCJxd6rZnZM" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+</script>
+<script type="text/xoomcoder">
+<iframe width="560" height="315" src="https://www.youtube.com/embed/er416Ad3R1g" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+</script>
+<script type="text/xoomcoder">
+<iframe width="560" height="315" src="https://www.youtube.com/embed/wSc0c5S00xY" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+</script>
+
+
