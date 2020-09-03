22b4be2d9a44bfee52089e697d47b8499d93e464
LH XoomCoder <github@xoomcoder.com>
2020-09-03 21:07:05 +0200

    studio: ajout panneau de commande

--- xoom-pages/studio.php
+++ xoom-pages/studio.php
@@ -24,20 +24,31 @@
         </header>
         <main>
             <section>
-            <h1>Tableau de Bord</h1>
+                <h1>Tableau de Bord</h1>
+                
+                <h2>TODO</h2>
+                <todo-item
+                    v-for="item in todos"
+                    v-bind:todo="item"
+                    v-bind:key="item.id"
+                ></todo-item>
+            </section>
+
+            <section>
 
                 <h2>Panneau de Commande</h2>
-                <form action="" @submit.prevent="">
+                <form action="" @submit.prevent="sendAjax">
                     <textarea id="batchcode" name="command" required cols="80" rows="10"></textarea>
                     <button id="batchbutton" type="submit">envoyer la commande</button>
                     <div class="feedback"></div>
                     <!-- partie technique -->
                     <input type="hidden" name="classApi" value="Member">
-                    <input type="hidden" name="methodApi" value="doCommand">
+                    <input type="hidden" name="methodApi" value="run">
                     <input type="hidden" name="loginToken" v-model="loginToken">
                 </form>
 
             </section>
+
         </main>
     </div>
     <script src="https://unpkg.com/vue@next"></script>
@@ -46,11 +57,40 @@
 const appConfig = {    
     data() {
         return {
+            todos: [
+                { id: 0, text: 'Level 1: Landing Page' },
+                { id: 1, text: 'Level 2: Site Vitrine' },
+                { id: 2, text: 'Level 3: Site Blog' },
+                { id: 3, text: 'Level 4: Site CMS' },
+                { id: 4, text: 'Level 5: Site Marketplace' }
+            ],
             username: '',
             loginToken: ''
         }
     },
-    methods: {
+    methods: {        // add here your functions/methods
+        sendAjax (event) {
+            var fd = new FormData(event.target);
+            fetch('api', {
+                method: 'POST',
+                body: fd
+            })
+            .then((response) => {
+                response
+                    .json()
+                    .then((json) => {
+                        var ajaxpack = {
+                            'event': event,
+                            'fd' : fd,
+                            'response': response,
+                            'json': json 
+                            };
+                        for(m in xcb) {
+                            xcb[m](ajaxpack);
+                        }
+                    })
+            });
+        },
         actLogout() {
             sessionStorage.setItem('loginToken', '');
             location.replace('login');   
@@ -71,7 +111,24 @@ const appConfig = {
 };
 var app = Vue.createApp(appConfig);
 
+app.component('todo-item', {
+  props: ['todo'],
+  template: `<li>{{ todo.text }}</li>`
+});
+
 app.mount('#app');   // css selector to link with HTML
+
+
+
+var xcb = {};
+// add callbacks to activate on ajax response
+xcb.feedback = function (ajaxpack)  {
+    if ('feedback' in ajaxpack.json) {
+        var f = ajaxpack.event.target.querySelector('.feedback');
+        if (f) f.innerHTML = ajaxpack.json.feedback;
+    }
+
+};
 
     </script>
 </body>
---
+++ xoom/class/api/ApiMember.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * author:  Long Hai LH
+ * date:    2020-09-03 21:05:06
+ * licence: MIT
+ */
+
+class ApiMember
+{
+    static function run ()
+    {
+        Form::setFeedback("Bienvenue...");
+
+    }
+    //@end
+}
