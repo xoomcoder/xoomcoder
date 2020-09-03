d2474e46b3c05af529b246efa0a4efdb91c6088b
LH XoomCoder <github@xoomcoder.com>
2020-09-03 20:51:51 +0200

    ajout panneau commande

--- xoom-pages/studio.php
+++ xoom-pages/studio.php
@@ -22,6 +22,23 @@
             <h1>BIENVENUE DANS VOTRE STUDIO </h1>
             <h2>({{ username }}) <small><a href="#logout" @click.prevent="actLogout">logout</a></small></h2>
         </header>
+        <main>
+            <section>
+            <h1>Tableau de Bord</h1>
+
+                <h2>Panneau de Commande</h2>
+                <form action="" @submit.prevent="">
+                    <textarea id="batchcode" name="command" required cols="80" rows="10"></textarea>
+                    <button id="batchbutton" type="submit">envoyer la commande</button>
+                    <div class="feedback"></div>
+                    <!-- partie technique -->
+                    <input type="hidden" name="classApi" value="Member">
+                    <input type="hidden" name="methodApi" value="doCommand">
+                    <input type="hidden" name="loginToken" v-model="loginToken">
+                </form>
+
+            </section>
+        </main>
     </div>
     <script src="https://unpkg.com/vue@next"></script>
     <script>
