d427036222f73390414a8c6193726b22797846ad
LH XoomCoder <github@xoomcoder.com>
2020-09-03 18:39:39 +0200

    typo

--- xoom/class/api/ApiUser.php
+++ xoom/class/api/ApiUser.php
@@ -221,6 +221,7 @@ class ApiUser
             $users = Model::read("user", "email", $email);
             foreach($users as $user) {
                 extract($user);
+                // FIXME: manage errors
                 list($payload, $signature) = @explode(",", @base64_decode($key));
                 if ( !empty($payload) && !empty($signature)
                         && password_verify("$payload$password", $signature) ) {
