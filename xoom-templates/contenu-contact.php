3652ac07c31d0662aea05827d870155c137f6118
LH XoomCoder <github@xoomcoder.com>
2020-09-10 13:23:37 +0200

    css

--- xoom-templates/contenu-contact.php
+++ xoom-templates/contenu-contact.php
@@ -6,11 +6,3 @@
     <p>Nous pouvons vous conseiller gratuitement.</p>
     <img class="h40" src="assets/img/team-640.jpg" alt="formation xoomcoder">            
 </section>
-
-<section class="nobg">
-    <h1>Formulaire de renseignements</h1>
-    <strong>Remplissez le formulaire suivant et recevez un code promo de 20% sur votre prochain achat sur le site.</strong>
-    <script type="text/xoomcoder">
-        <iframe title="formulaire de contact" src="https://docs.google.com/forms/d/e/1FAIpQLSdz3ZjxG5lYBoV7vwnab00pnldHRl92tRVq9ydH-0WCeIiMug/viewform?embedded=true" width="10%" height="3600" frameborder="0" marginheight="0" marginwidth="0">Chargement…</iframe>   
-    </script>
-</section>
