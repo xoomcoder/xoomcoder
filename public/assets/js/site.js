2597d4347ba7366bd879d02aa174a84dd9c49ab5
LH XoomCoder <github@xoomcoder.com>
2020-09-10 10:34:47 +0200

    ajout cookie screen

--- public/assets/js/site.js
+++ public/assets/js/site.js
@@ -34,6 +34,30 @@ function cbAjax (event)
     })
 }
 
+// https://www.w3schools.com/js/js_cookies.asp
+function setCookie(cname, cvalue, exdays) {
+    var d = new Date();
+    d.setTime(d.getTime() + (exdays*24*60*60*1000));
+    var expires = "expires="+ d.toUTCString();
+    document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+}
+
+function getCookie(cname) {
+    var name = cname + "=";
+    var decodedCookie = decodeURIComponent(document.cookie);
+    var ca = decodedCookie.split(';');
+    for(var i = 0; i <ca.length; i++) {
+      var c = ca[i];
+      while (c.charAt(0) == ' ') {
+        c = c.substring(1);
+      }
+      if (c.indexOf(name) == 0) {
+        return c.substring(name.length, c.length);
+      }
+    }
+    return "";
+}
+
 function run() {
     // do something... 2000ms later
 
@@ -49,6 +73,14 @@ function run() {
 
     // install ajax
     addAction("form[action=api]", "submit", cbAjax);
+
+    // keep screen resolution in cookies
+    let scook = {
+        w: screen.width,
+        h: screen.height,
+    };
+    setCookie("screen", JSON.stringify(scook), 1);
+
 }
 
 run();
