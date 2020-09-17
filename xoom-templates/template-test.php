103d33f06c8aa3adea08e7f6f9f41bf690d46e09
LH XoomCoder <github@xoomcoder.com>
2020-09-17 14:50:37 +0200

    debg

--- xoom-templates/template-test.php
+++ xoom-templates/template-test.php
@@ -5,10 +5,15 @@ extract($geocms);
 
 include __DIR__ . "/header.php";
 
-$article = News::buildHtml($geocms, false);       
+$article = News::buildHtml($geocms, false); 
+$debug = json_encode($geocms, JSON_PRETTY_PRINT);
+
 echo 
 <<<x
     <h1>TEST</h1>
+    <pre>
+    $debug
+    </pre>
     <section class="single">
     $article
     </section>
