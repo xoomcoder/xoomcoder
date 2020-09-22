50e0b9da61f5c3ab11d0539f80ada750e936c79b
LH XoomCoder <github@xoomcoder.com>
2020-09-23 00:50:06 +0200

    debug

--- public/assets/js/site.js
+++ public/assets/js/site.js
@@ -77,7 +77,8 @@ function run() {
             var parsedUrl = new URL(current.innerHTML);
             const youtubeId = parsedUrl.searchParams.get('v');
             // https://developer.mozilla.org/fr/docs/Web/API/Element/insertAdjacentHTML
-            current.parentNode.innerHTML = `<iframe title="youtube" width="100%" height="315" src="https://www.youtube.com/embed/${youtubeId}"></iframe>`;
+            current.parentNode.insertAdjacentHTML('beforebegin', 
+            `<iframe title="youtube" width="100%" height="315" src="https://www.youtube.com/embed/${youtubeId}"></iframe>`);
         }   
     }, 4000);
 
